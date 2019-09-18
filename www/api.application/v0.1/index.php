95d00eeaa031e44928bb7f18f5d4153977a99abd
Ayesh Alshukri <a.alshukri@liverpool.ac.uk>
2019-09-18 11:40:19 +0100

    api with default test response.

--- www/api.application/v0.1/index.php
+++ www/api.application/v0.1/index.php
@@ -1,6 +1,18 @@
 <?php 
-/*
- *
+/**
+ * API v0.1
+ * Application
+ * 2019-09-18 11:09:17 
  */
-?>
-test
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// set response code - 200 OK
+http_response_code(200);
+
+// tell the user no products found
+echo json_encode(
+	array("message" => "test.")
+);
