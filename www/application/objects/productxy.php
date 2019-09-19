e6314532484e47cab6b68434d416d7b4fc507910
Ayesh Alshukri <a.alshukri@liverpool.ac.uk>
2019-09-19 11:31:31 +0100

    stub for running productXY r

---
+++ www/application/objects/productxy.php
@@ -0,0 +1,16 @@
+<?php
+// RInterface object
+class ProductXY{
+ 
+	// private 
+	private $rScript = "productxy.r";
+
+
+	// constructor
+	public function __construct()
+	{}
+ 
+	// emailExists
+	public function product()
+	{}
+} 
