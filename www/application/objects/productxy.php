ee70f7174d883a375a0e5290ebee5ae8f9fe6214
Ayesh Alshukri <a.alshukri@liverpool.ac.uk>
2019-09-20 14:53:15 +0100

    Tidy up code dev comments.

--- www/application/objects/productxy.php
+++ www/application/objects/productxy.php
@@ -14,18 +14,7 @@ class ProductXY{
 	public function product($x,$y)
 	{
 		$cmd = "Rscript ".dirname(__FILE__)."/".$this->rScript." $x $y";
-	
-		//exec($cmd,$output,$return_var);
 		$output = shell_exec($cmd);
-		//$output = shell_exec("Rscript /var/www/html/application/objects/productxy.r 10 20 2>$1");
-		//$output = shell_exec("Rscript /var/www/html/application/objects/productxy.r 10 20");
-
-		//print_r($cmd);
-		//echo"\n";
-		//print_r($output);
-		//echo"\n";
-		//print_r($return_var);
-		//echo"\n";
 	
 		$z = explode(" ", $output)[1];
 
